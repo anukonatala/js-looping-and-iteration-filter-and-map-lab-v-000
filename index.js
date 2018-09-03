be90138ce947cb8ff4ef005b432066b4470ae5d1
Anusha Konatala <anu.konatala@gmail.com>
2018-09-03 18:11:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function driverNamesWithRevenueOver(drivers, revenue) {
     return driver.revenue > parseInt(revenue)}).map(function(driver) {return driver.name});
 }
 
-function attributeFilter(driver) {
+function attributeFilter(driver,  {key: value}) {
   if (driver.hasOwnProperty('key') && driver['key'] === value) {
     return driver;
   }
