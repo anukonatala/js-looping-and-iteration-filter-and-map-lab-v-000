572a1284610a8b1744772e87c22833613977d9a2
Anusha Konatala <anu.konatala@gmail.com>
2018-09-03 18:07:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here:
+
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){ 
+    return parseInt(driver.revenue) > parseInt(revenue)
+  });
+}
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function(driver){
+    return driver.revenue > parseInt(revenue)}).map(function(driver) {return driver.name});
+}
+
+function attributeFilter(driver) {
+  if (driver.hasOwnProperty('key') && driver['key'] === value) {
+    return driver;
+  }
+}
+
+function exactMatch(drivers, {key: value}) {
+  return drivers.filter(attributeFilter);
+}
